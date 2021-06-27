f01ff4d25eeabf0d8bdae1f99257fb204201acbc
nahimanajz <jazzonahimana@gmail.com>
2021-06-27 22:55:20 +0200

    change theme

--- js/script.js
+++ js/script.js
@@ -1,29 +1,71 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  // Load data when page loads
-})
+  /** 
+    
+      @params {String} name of tag to be created
+      @return {HtmlElement} created tag
+  */
 
-/**
- * @param{URL} url to fetch users or posts
+ const newTag=(tagName)=> document.createElement(tagName);
+ 
+ /** 
+      @params {String} name CSS class to be retrieved
+      @return {HtmlElement} retreived tag with child appended to it
+  */
+
+ const getTag=(parentElementClass,childTag)=> document.getElementsByClassName(parentElementClass)[0].appendChild(childTag)
+ 
+ /** 
+    @params {String} type of data to retrieve data [it might be user or posts]
+    @return {HtmlEleme} all posts of chosen user
  */
-const getData = async(url) =>{
-    return await fetch('https://jsonplaceholder.typicode.com/users')
-  .then((response) => response.json())
-  
+
+const getUserPosts = (id) =>{
+ const ul = newTag('ul')
+
+ fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`).
+ then(posts => posts.json()).
+ then(posts => posts.forEach(({title})=>{
+
+   const li = newTag('li')
+   getTag('posts', ul)
+   li.innerHTML= title
+   ul.appendChild(li)
+ }))
 }
-//todo fetch data 
-// append them on list of users
-//get html ul to 
-const users= document.querySelector(".user")
-  
-
-getData().then((response) =>{
-    response.forEach(({email, name,id}) =>{  
-       const user = document.createElement('li')
-       user.innerText = name
-       user.classList.add('list-item')
-       users.('<li>hello</li>')
-
-        })
-    });
 
 
+function getData() {
+fetch('https://jsonplaceholder.typicode.com/users',).
+then(data => data.json()).
+then(data => {
+   console.log(data)
+    data.forEach(({name, email, id})=>{
+   var div = newTag('div')
+     getTag('user', div);
+
+     let listName = newTag('h2')
+     let listEmail = newTag('p')
+     let userPostBtn = newTag('button')
+     userPostBtn.innerHTML = 'Get User’s Posts'   
+     userPostBtn.addEventListener('click', function(e) {
+         const list = document.querySelector('ul')
+         if(list){
+             list.parentNode.removeChild(list)
+         }
+         getUserPosts(id)
+     })
+    
+     div.appendChild(listName)
+     div.appendChild(listEmail)
+     div.appendChild(userPostBtn)
+     listEmail.innerHTML = email
+     listName.innerHTML=name
+    
+ })}).catch(e => {
+ console.log(e)
+})
+}
+
+getData()
+
+})
